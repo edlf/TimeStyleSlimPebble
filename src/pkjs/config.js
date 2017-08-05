ee3731223bae0a61d66f390e329d5a2d059b55d7
Eduardo Fernandes <e.luis@icloud.com>
2017-08-05 16:12:41 +0100

    Add empty clay config page

---
+++ src/pkjs/config.js
@@ -0,0 +1,10 @@
+module.exports = [{
+  "type": "section",
+  "items": [{
+    "type": "heading",
+    "defaultValue": "TimeStyleSlim config"
+  }, {
+    "type": "submit",
+    "defaultValue": "SEND"
+  }]
+}]
